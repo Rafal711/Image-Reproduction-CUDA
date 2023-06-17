5fe4af2930b8de8c859a848540dfbe0099f99ee7
Jaromir-hub146 <62473586+Jaromir-hub146@users.noreply.github.com>
2023-06-17 20:47:51 +0200

    Add header to cvtColor method

--- image_reproduction.cu
+++ image_reproduction.cu
@@ -6,6 +6,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
